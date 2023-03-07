716c15c3998a00eb6b673bc4bf88a6c2da8245a8
jiheon788 <jiheon788@ajou.ac.kr>
2023-03-08 03:55:32 +0900

    feat: axios interceptor

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,9 +1,52 @@
-import axios from 'axios';
+import axios, {
+  AxiosResponse,
+  InternalAxiosRequestConfig,
+  AxiosError,
+} from 'axios';
 
 const HOST = 'http://localhost:3000';
 
 const apiClient = axios.create({
   baseURL: HOST,
 });
+
+const logOnDev = (
+  message: string,
+  log?: AxiosResponse | InternalAxiosRequestConfig | AxiosError,
+) => {
+  if (process.env.NODE_ENV === 'development') {
+    console.log(message, log);
+  }
+};
+
+apiClient.interceptors.request.use((request) => {
+  const { method, url } = request;
+
+  logOnDev(`🚀 [${method?.toUpperCase()}] ${url} | Request`, request);
+
+  return request;
+});
+
+apiClient.interceptors.response.use(
+  (response) => {
+    const { method, url } = response.config;
+    const { status } = response;
+
+    logOnDev(
+      `✨ [${method?.toUpperCase()}] ${url} | Response ${status}`,
+      response,
+    );
+
+    return response;
+  },
+  (error) => {
+    const { message } = error;
+    const { method, url } = error.config;
+
+    logOnDev(`🚨 [${method?.toUpperCase()}] ${url} | Error ${message}`, error);
+
+    return Promise.reject(error);
+  },
+);
 
 export default apiClient;
